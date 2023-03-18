a2010a89e64e01ed246bbab22c12cc2544a17797
johan12345 <johan.forstner@gmail.com>
2023-03-18 18:08:15 +0100

    add support for multiple cameras and GH3 (untested)

--- video_time_webui.py
+++ video_time_webui.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-from lumix_control import CameraControl
-import xml.etree.ElementTree as ET
+
 import datetime as dt
+import json
 import threading
 import time
+import xml.etree.ElementTree as ET
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
 
-IP = "192.168.50.11"
+from lumix_control import CameraControl
+
+IP = ["192.168.50.11", "192.168.50.12"]
 state = {
-    'should_record': False
+    'should_record': False,
+    'cameras': {i: {} for i in IP}
 }
 
 
@@ -17,29 +20,53 @@ def camera_control_thread(ip, state):
     control = CameraControl(ip)  # IP of camera
 
     while True:
-        state['connected'] = False
+        my_state = state['cameras'][ip]
+        my_state['connected'] = False
         try:
             control.start_camera_control()
-            state['connected'] = True
-            
+            my_state['connected'] = True
+
+            prev_remaining = dt.timedelta(hours=99)
+            started = False
             while True:
                 cam_state = ET.fromstring(control.get_state().text).find('state')
                 remaining = dt.timedelta(seconds=int(cam_state.find('video_remaincapacity').text))
-                rec = cam_state.find('rec').text == 'on'
-                
-                state['rec'] = rec
-                state['remaining'] = remaining.total_seconds()
-                
-                if state['should_record']:
-                    should_restart = not rec or remaining < dt.timedelta(seconds=10)
+
+                rec_elem = cam_state.find('rec')
+                if rec_elem is not None:
+                    # G81
+                    rec = rec_elem.text == 'on'
+                else:
+                    rec = None
+
+                my_state['rec'] = rec
+                my_state['remaining'] = remaining.total_seconds()
+
+                should_record = state['should_record']
+                if should_record:
+                    if rec is not None:
+                        # G81
+                        should_restart = not rec or remaining < dt.timedelta(seconds=10)
+                    else:
+                        # GH3
+                        should_restart = remaining > prev_remaining + dt.timedelta(minutes=1) or not started
                     if should_restart:
                         print('restarting record for {}'.format(ip))
-                        control.video_record_stop()
+                        try:
+                            control.video_record_stop()
+                        except:
+                            pass
                         control.video_record_start()
+                        started = True
                 else:
                     if rec:
                         print('stopping recording for {}'.format(ip))
-                        control.video_record_stop()   
+                        try:
+                            control.video_record_stop()
+                        except:
+                            pass
+                        started = False
+                prev_remaining = remaining
                 time.sleep(1)
         except:
             time.sleep(5)
@@ -74,9 +101,10 @@ class WebUiServer(BaseHTTPRequestHandler):
                 state['should_record'] = False
         else:
             self.send_response(404)
-        
-thread = threading.Thread(target=camera_control_thread, args=(IP, state), daemon=True)
-thread.start()
+
+for ip in IP:
+    thread = threading.Thread(target=camera_control_thread, args=(ip, state), daemon=True)
+    thread.start()
         
 server = HTTPServer(("0.0.0.0", 8000), WebUiServer)
 print("Server starting")
